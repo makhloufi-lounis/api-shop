937b591bb47d272fa9d9a371b4d1826076898fb7
Lounis Makhloufi <makhloufi.lounis@gmail.com>
2020-06-06 16:45:28 +0200

    installation and configuration of PHP_CodeSniffer

---
+++ .idea/fileTemplates/includes/PHP File Header.php
@@ -0,0 +1,8 @@
+/**
+ * API Shop
+ *
+ * @version   1.0
+ * @author    Lounis Makhloufi <makhloufi.lounis@gmail.com>
+ * @see       https://github.com/makhloufi-lounis/api-shop for the canonical source repository
+ * @copyright Copyright (c) 2020. 
+ */
