3a729b6f7dd1bb0a819f27bba3f099de2e78e2f0
Lounis Makhloufi <makhloufi.lounis@gmail.com>
2020-06-10 03:06:35 +0200

    add checkout page

--- assets/js/App.jsx
+++ assets/js/App.jsx
@@ -5,8 +5,9 @@ import HomePage from './pages/HomePage';
 import {HashRouter, Switch, Route} from 'react-router-dom';
 import Navbar from './Componants/Navbar';
 import CartPage from './pages/CartPage';
-import Default from './pages/Default';
-import Footer from './Componants/Footer'
+import DefaultPage from './pages/DefaultPage';
+import Footer from './Componants/Footer';
+import CheckoutPage from './pages/CheckoutPage';
 
 const App = (props) => {
     const { items, saveLocalStorage } = props;
@@ -26,21 +27,23 @@ const App = (props) => {
 
     return (
         <>
-            <HashRouter>
-                <Navbar />            
-                <main>
-                    <Switch>
-                        <Route exact path="/"> 
-                            <HomePage qty={qty} setQty={setQty} add={add}/>
-                        </Route>
-                        <Route path="/cart">
-                            <CartPage qty={qty} setQty={setQty} />
-                        </Route>
-                        <Route component={Default} />
-                                    
-                    </Switch>
-                </main>
-                <Footer />
+            <HashRouter>               
+                    <Navbar />            
+                    <main>
+                        <Switch>
+                            <Route exact path="/"> 
+                                <HomePage qty={qty} setQty={setQty} add={add}/>
+                            </Route>
+                            <Route path="/cart">
+                                <CartPage qty={qty} setQty={setQty} />
+                            </Route>
+                            <Route path="/checkout">
+                                <CheckoutPage />
+                            </Route>
+                            <Route component={DefaultPage} />                                    
+                        </Switch>
+                    </main>
+                    <Footer />
             </HashRouter>
         </>
     )
--- assets/js/pages/Default.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Default = () => {
-    return ( 
-        <h3>Page not found !</h3>
-     );
-}
- 
-export default Default;
